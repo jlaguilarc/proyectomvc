9fb627f6cdbe35795969d5d266c919a1880c99c3
jlaguilarc <jos05_12@hotmail.com>
2023-03-29 18:12:50 -0500

    Update database.php

--- Config/database.php
+++ Config/database.php
@@ -2,7 +2,7 @@
     class Conexion
     {
         public static function Conexion(){
-            $conexion = new mysqli("localhost","root","","proyectomvc");
+            $conexion = new mysqli("containers-us-west-189.railway.app","root","TvfFvOp4XwfTnkS6YlwJ","railway");
             return $conexion;
         }
 
