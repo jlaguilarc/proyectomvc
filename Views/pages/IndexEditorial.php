bc3fcfc755ebbde93daac6e4714c61c2181cffc5
Jose Aguilar <jlaguilarc@misena.edu.co>
2023-04-12 20:56:22 -0500

    Ruta Ajustadas para Editorial

--- Views/pages/IndexEditorial.php
+++ Views/pages/IndexEditorial.php
@@ -1,6 +1,6 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT']."/Views/layout/header.php");
-    require_once($_SERVER['DOCUMENT_ROOT']."./Controllers/EditorialController.php");
+    require_once($_SERVER['DOCUMENT_ROOT']."/Controllers/EditorialController.php");
 ?>
 <h2>Página de Editoriales</h2>
 
